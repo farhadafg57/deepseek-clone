8ee5ad9ad339615df0ae71877bfb22c6add8f56d
elyse502 <elyseniyibizi502@gmail.com>
2025-03-18 15:19:42 +0200

    Add Login functionality using clerk authentication

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { useClerk, UserButton } from "@clerk/nextjs";
+import { useAppContext } from "@/context/AppContext";
 
 const Sidebar = ({ expand, setExpand }) => {
+  const { openSignIn } = useClerk();
+  const { user } = useAppContext();
+
   return (
     <div
       className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${
@@ -118,11 +123,21 @@ const Sidebar = ({ expand, setExpand }) => {
         </div>
 
         <div
+          onClick={user ? null : openSignIn}
           className={`flex items-center ${
             expand ? "hover:bg-white/10 rounded-lg" : "justify-center w-full"
           } gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}
         >
-          <Image src={assets.profile_icon} alt="profile_icon" className="w-7" />
+          {user ? (
+            <UserButton />
+          ) : (
+            <Image
+              src={assets.profile_icon}
+              alt="profile_icon"
+              className="w-7"
+            />
+          )}
+
           {expand && <span>My Profile</span>}
         </div>
       </div>
